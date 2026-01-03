10d05f7f3f9fe0b67f025f4db8ec1ee825e26e89
Andrey Bozin <andrei_bozin@mail.ru>
2026-01-03 22:12:30 +0500

    Создание меню и добавление экранов

--- mobile/NoiseMonitor/app/src/main/java/com/example/noisemonitor/MainActivity.kt
+++ mobile/NoiseMonitor/app/src/main/java/com/example/noisemonitor/MainActivity.kt
@@ -1,18 +1,38 @@
 package com.example.noisemonitor
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.noisemonitor.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            openFragment(DashboardFragment())
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_monitor -> openFragment(DashboardFragment())
+                R.id.nav_history -> openFragment(HistoryFragment())
+                R.id.nav_alerts -> openFragment(AlertsFragment())
+                R.id.nav_device -> openFragment(DeviceFragment())
+                else -> false
+            }
+            true
+        }
     }
 }
